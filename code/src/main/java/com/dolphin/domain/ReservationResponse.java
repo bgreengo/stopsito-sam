dfeae375435ca769cd4614ee92f6ba13e7ae396c
George Kampanos <george.kampanos@thehutgroup.com>
2019-03-04 17:30:46 +0000

    Save request to dynamo

--- code/src/main/java/com/dolphin/domain/ReservationResponse.java
+++ code/src/main/java/com/dolphin/domain/ReservationResponse.java
@@ -5,23 +5,17 @@ import java.util.Objects;
 public final class ReservationResponse {
 
     private final String id;
-    private final long timestamp;
 
     public ReservationResponse() {
-        this("", 0);
+        this("");
     }
 
-    public ReservationResponse(String id, long timestamp) {
+    public ReservationResponse(String id) {
         this.id = id;
-        this.timestamp = timestamp;
     }
 
     public String getId() {
         return id;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
     }
 
     @Override
@@ -33,20 +27,18 @@ public final class ReservationResponse {
             return false;
         }
         ReservationResponse that = (ReservationResponse) o;
-        return Objects.equals(id, that.id) &&
-            Objects.equals(timestamp, that.timestamp);
+        return Objects.equals(id, that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, timestamp);
+        return Objects.hash(id);
     }
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("ReservationResponse{");
         sb.append("id='").append(id).append('\'');
-        sb.append(", timestamp=").append(timestamp);
         sb.append('}');
         return sb.toString();
     }
--- code/src/main/java/com/dolphin/handler/ReservationHandler.java
+++ code/src/main/java/com/dolphin/handler/ReservationHandler.java
@@ -4,30 +4,37 @@ import com.amazonaws.handlers.RequestHandler2;
 import com.dolphin.domain.ReservationRequest;
 import com.dolphin.domain.ReservationResponse;
 import com.dolphin.exception.ReservationMissingFieldsException;
-import java.util.UUID;
+import com.dolphin.service.PersistenceService;
 
 public final class ReservationHandler extends RequestHandler2 {
+
+    private static final PersistenceService persistenceService = new PersistenceService();
 
     public ReservationResponse handleRequest(ReservationRequest request) throws ReservationMissingFieldsException {
         if (null == request) {
             throw new IllegalStateException("Reservation Request cannot be null");
         }
 
-        if (isRequestMissingNameOrEmailFields(request)) {
+        if (isRequestMissingRequiredFields(request)) {
             throw new ReservationMissingFieldsException();
         }
-
-        
-
-        return new ReservationResponse(UUID.randomUUID().toString(), request.getTimestamp());
+        return persistenceService.put(request);
     }
 
-    public boolean isRequestMissingNameOrEmailFields(ReservationRequest request) {
+    private boolean isRequestMissingRequiredFields(ReservationRequest request) {
+        return isRequestMissingNameOrEmailFields(request) || isRequestMissingPartyOrTimestampFields(request);
+    }
+
+    private boolean isRequestMissingNameOrEmailFields(ReservationRequest request) {
         return isStringBlank(request.getEmail()) || isStringBlank(request.getName());
     }
 
-    private boolean isStringBlank(String email) {
-        return null == email || email.trim().length() == 0;
+    private boolean isRequestMissingPartyOrTimestampFields(ReservationRequest request) {
+        return request.getParty() < 1 || request.getTimestamp() < 1;
+    }
+
+    private boolean isStringBlank(String s) {
+        return null == s || s.trim().length() == 0;
     }
 
 }
