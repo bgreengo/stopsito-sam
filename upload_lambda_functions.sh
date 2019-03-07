c2e7bd1d3d51f393ff16ef92912b800fadba6495
George Kampanos <george.kampanos@thehutgroup.com>
2019-03-07 13:35:40 +0000

    Cloudformation refactoring and create dyanamodb stream lambda functions

--- upload_lambda_functions.sh
+++ upload_lambda_functions.sh
@@ -4,6 +4,13 @@ GOOS=linux go build ./lambda-functions/src/create-reservation/main.go
 zip create-reservation.zip main
 rm main
 
+GOOS=linux go build ./lambda-functions/src/publish-reservation-event/main.go
+zip publish-reservation-event.zip main
+rm main
+
 aws cloudformation deploy --template-file cloudformation/lambda-functions-bucket.template --profile stopsito --stack-name stopsito-lambda-bucket
 aws s3 cp create-reservation.zip s3://sto-psito-lambda-functions/ --profile stopsito
+aws s3 cp publish-reservation-event.zip s3://sto-psito-lambda-functions/ --profile stopsito
+
 rm create-reservation.zip
+rm publish-reservation-event.zip
