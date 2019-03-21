94866e8821b8c246f934dca809ddc449a68b323c
George Kampanos <george.kampanos@thehutgroup.com>
2019-03-21 13:50:22 +0000

    Cleanup deployment scripts

--- deployment/deploy_infratructure.sh
+++ deployment/deploy_infratructure.sh
@@ -4,17 +4,17 @@ AWS_PROFILE=$1
 
 echo "[+] Creating DynamoDb Tables"
 {
-    aws cloudformation deploy --template-file ../cloudformation/create-dynamodb-tables.template --profile ${AWS_PROFILE} --stack-name stopsito-dynamodb-tables
+    aws cloudformation deploy --template-file ./cloudformation/create-dynamodb-tables.template --profile ${AWS_PROFILE} --stack-name stopsito-dynamodb-tables
 } &> /dev/null
 
 echo "[+] Creating Api Gateway Endpoints"
 {
-    aws cloudformation deploy --template-file ../cloudformation/create-api-gateway.template --profile ${AWS_PROFILE} --stack-name stopsito-api-endpoints
+    aws cloudformation deploy --template-file ./cloudformation/create-api-gateway-endpoints.template --profile ${AWS_PROFILE} --stack-name stopsito-api-endpoints
 } &> /dev/null
 
 echo "[+] Creating SNS Topics"
 {
-    aws cloudformation deploy --template-file ../cloudformation/create-sns-topics.template --profile ${AWS_PROFILE} --stack-name stopsito-sns-topics
+    aws cloudformation deploy --template-file ./cloudformation/create-sns-topics.template --profile ${AWS_PROFILE} --stack-name stopsito-sns-topics
 } &> /dev/null
 
 echo "[!] Finished!"
--- deployment/deploy_lambda_functions.sh
+++ deployment/deploy_lambda_functions.sh
@@ -4,27 +4,31 @@ AWS_PROFILE=$1
 
 echo "[+] Building Lambda Functions"
 {
-    GOOS=linux go build ../lambda-functions/src/create-reservation/main.go
+    GOOS=linux go build ./lambda-functions/src/create-reservation/main.go
     zip create-reservation.zip main
     rm main
 
-    GOOS=linux go build ../lambda-functions/src/publish-reservation-event/main.go
+    GOOS=linux go build ./lambda-functions/src/publish-reservation-event/main.go
     zip publish-reservation-event.zip main
     rm main
 
-    GOOS=linux go build ../lambda-functions/src/send-sms/main.go
+    GOOS=linux go build ./lambda-functions/src/send-sms/main.go
     zip send-sms.zip main
     rm main
 
-    GOOS=linux go build ../lambda-functions/src/send-confirmation-emails/main.go
+    GOOS=linux go build ./lambda-functions/src/send-confirmation-emails/main.go
     zip send-confirmation-emails.zip main
     rm main
 
 } &> /dev/null
 
+echo "[+] Creating S3 bucket"
+{
+    aws cloudformation deploy --template-file ./cloudformation/create-s3-buckets.template --profile ${AWS_PROFILE} --stack-name stopsito-s3-buckets
+} &> /dev/null
+
 echo "[+] Uploading Lambda Functions to S3"
 {
-    aws cloudformation deploy --template-file ../cloudformation/create-s3-buckets.template --profile ${AWS_PROFILE} --stack-name stopsito-s3-buckets
     aws s3 cp create-reservation.zip s3://sto-psito-lambda-functions/ --profile ${AWS_PROFILE}
     aws s3 cp publish-reservation-event.zip s3://sto-psito-lambda-functions/ --profile ${AWS_PROFILE}
     aws s3 cp send-sms.zip s3://sto-psito-lambda-functions/ --profile ${AWS_PROFILE}
